16e64e577c5c3be56fb266529f7097cf93d4c6e0
Rami Martin <rami.34000@gmail.com>
2021-11-19 19:28:57 +0100

    Update Builder

--- rs2_common_lib/src/main/java/RS2AndroidBuilder.java
+++ rs2_common_lib/src/main/java/RS2AndroidBuilder.java
@@ -10,50 +10,50 @@ public class RS2AndroidBuilder {
     public static void main(String[] args) throws Exception {
 
         String libName = "rs2";
-
+        String[] classes = new String[]{
+                "**/RsContext.java",
+                "**/DeviceList.java",
+                "**/Device.java",
+                "**/Pipeline.java",
+                "**/PipelineProfile.java",
+                "**/Config.java",
+                "**/Frame.java",
+                "**/FrameSet.java",
+                "**/FrameQueue.java",
+                "**/DepthFrame.java",
+                "**/VideoFrame.java",
+                "**/Points.java",
+                "**/StreamProfile.java",
+                "**/Sensor.java",
+                "**/Options.java",
+                "**/ProcessingBlock.java",
+                "**/Align.java",
+                "**/Colorizer.java",
+                "**/DecimationFilter.java",
+                "**/DisparityTransformFilter.java",
+                "**/HoleFillingFilter.java",
+                "**/Pointcloud.java",
+                "**/SpatialFilter.java",
+                "**/TemporalFilter.java",
+                "**/ThresholdFilter.java",
+                "**/ZeroOrderInvalidationFilter.java",
+                "**/YuyDecoder.java",
+                "**/HdrMerge.java",
+                "**/SequenceIdFilter.java",
+                "**/DepthSensor.java",
+                "**/RoiSensor.java",
+                "**/VideoStreamProfile.java",
+                "**/MotionStreamProfile.java",
+                "**/Utils.java",
+                "**/FwLogger.java",
+                "**/FwLogMsg.java",
+                "**/FwLogParsedMsg.java",
+        };
         new NativeCodeGenerator().generate(
                 "src/main/java/",
                 "build/classes/java/main",
                 "jni/realsense",
-                new String[]{
-                        "**/RsContext.java",
-                        "**/DeviceList.java",
-                        "**/Device.java",
-                        "**/Pipeline.java",
-                        "**/PipelineProfile.java",
-                        "**/Config.java",
-                        "**/Frame.java",
-                        "**/FrameSet.java",
-                        "**/FrameQueue.java",
-                        "**/DepthFrame.java",
-                        "**/VideoFrame.java",
-                        "**/Points.java",
-                        "**/StreamProfile.java",
-                        "**/Sensor.java",
-                        "**/Options.java",
-                        "**/ProcessingBlock.java",
-                        "**/Align.java",
-                        "**/Colorizer.java",
-                        "**/DecimationFilter.java",
-                        "**/DisparityTransformFilter.java",
-                        "**/HoleFillingFilter.java",
-                        "**/Pointcloud.java",
-                        "**/SpatialFilter.java",
-                        "**/TemporalFilter.java",
-                        "**/ThresholdFilter.java",
-                        "**/ZeroOrderInvalidationFilter.java",
-                        "**/YuyDecoder.java",
-                        "**/HdrMerge.java",
-                        "**/SequenceIdFilter.java",
-                        "**/DepthSensor.java",
-                        "**/RoiSensor.java",
-                        "**/VideoStreamProfile.java",
-                        "**/MotionStreamProfile.java",
-                        "**/Utils.java",
-                        "**/FwLogger.java",
-                        "**/FwLogMsg.java",
-                        "**/FwLogParsedMsg.java",
-                },
+                classes,
                 new String[]{
                         "**/RS2AndroidBuilder.java",
                         "**/RS2DesktopBuilder.java"
@@ -84,6 +84,7 @@ public class RS2AndroidBuilder {
         android.cIncludes = cIncludes;
         android.cppIncludes = cppIncludes;
         android.cppExcludes = excludes;
+        android.androidNdkSuffix = ".cmd";
         android.androidABIs = new String[]{"arm64-v8a", "armeabi-v7a", "x86", "x86_64"};
         android.androidSharedLib = "LOCAL_STATIC_LIBRARIES := librealsense2";
         android.androidAndroidMk = new String[]{
